d1edb3350d9d7b7dbd4e6d52bb45a09d270e20da
van <nguyenthevann6@gmail.com>
2023-09-26 23:44:26 +0700

    a

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv');
 const db = require('../models');
 const User = db.User;
 dotenv.config();
+// qqq
 const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
 const API_KEY_CHECK_MAIL = process.env.API_KEY_CHECK_MAIL;
